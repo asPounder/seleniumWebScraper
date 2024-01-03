4d37725511ba47f82d2dd07cb759b2afa617c0ef
asPounder <karlossokolowski75@gmail.com>
2024-01-03 04:00:47 +0100

    Redesigned for new version

--- src/App.java
+++ src/App.java
@@ -6,66 +6,82 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.ArrayList;
 import java.nio.file.*;
 
 import exceptions.*;
 
+/*
+ * TODO:
+ * 1. add gui
+ * 2. add caching
+ * 3. add config
+ */
+
 public class App {
-    public static void main(String[] args) {
+    public static List<List<String>> timetableScraper(
+        int timeframe, String login, String password) {
+
+        // - use the passed arguments
+        // - move declarations here for clarity 
+        // - rework exceptions
+        List<List<String>> output = new ArrayList<>();
 
         System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/EDriver/msedgedriver.exe");
         WebDriver driver = new EdgeDriver(new EdgeOptions().addArguments("--headless"));
 
         driver.get("https://extranet.vizja.net/");
 
-        WebElement login = driver.findElement(By.name("login"));
-        WebElement password = driver.findElement(By.name("haslo"));
+        WebElement loginEl = driver.findElement(By.name("login"));
+        WebElement passwordEl = driver.findElement(By.name("haslo"));
 
         try {
             List<String> data = Files.readAllLines(Paths.get("src\\credentials\\passwordAndLogin.txt"));
-            login.sendKeys(data.get(0));
-            password.sendKeys(data.get(1));
+            loginEl.sendKeys(data.get(0));
+            passwordEl.sendKeys(data.get(1));
         } catch (Exception e) {
             driver.quit();
             throw new NoLoginDataException("Invalid data in passwordAndLogin file");
         }
-        password.submit();
+        passwordEl.submit();
 
         if (driver.getCurrentUrl().equals("https://extranet.vizja.net/")) {
             driver.quit();
             throw new LoginFailureException("Invalid login credentials");
         }
-        WebElement next = driver.findElement(By.cssSelector(
-            "div.fc-button-group > button.fc-corner-right"));
+
         WebElement date = driver.findElement(By.cssSelector("div.fc-center > h2"));
+        output.add(new ArrayList<>(List.of(date.getText())));
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(50));
+        WebElement next = driver.findElement(By.cssSelector(
+            "div.fc-button-group > button.fc-corner-right"));
         WebElement timeTable = null;
-        final int TIMEFRAME = 100;
         int days = 0;
         while (timeTable == null) {
             try {
                 timeTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("table.fc-list-table")));
             } catch (Exception e) {
                 next.click();
-                if (days > TIMEFRAME) {
+                if (days > timeframe) {
                     driver.quit();
-                    throw new NoTimetableException("No timetable in specifiec timeframe.");
+                    throw new NoSuchElementException("No timetable in specifiec timeframe.");
                 }
                 days += 1;
             }
         }
 
-        System.out.println("Success! Printing...\n");
-        System.out.println("data: " + date.getText());
-
         List<WebElement> lessons = timeTable.findElements(By.cssSelector("tr.fc-list-item"));
         for (WebElement lesson : lessons) {
             String time = lesson.findElement(By.cssSelector("td.fc-list-item-time")).getText();
             String subject = lesson.findElement(By.cssSelector("td.fc-list-item-title > a")).getText();
-            System.out.println(subject + " " + time);
+            output.add(new ArrayList<>(List.of(subject + " " + time)));
         }
 
         driver.quit();
+        return output;
+    }
+    public static void main(String[] args) {
+        
     }
 }
