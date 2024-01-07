754efc2d67c325ab93a74ca6ddd2fdfb9ad130ca
asPounder <karlossokolowski75@gmail.com>
2024-01-07 20:32:35 +0100

    progress #3

--- src/App.java
+++ src/App.java
@@ -1,22 +1,19 @@
-import org.openqa.selenium.*;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Properties;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.NoSuchElementException;
+import java.util.Locale;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import javax.swing.*;
-
 
 /*
  * TODO:
@@ -27,72 +24,20 @@ import javax.swing.*;
  */
 
 public class App {
-    private static List<List<String>> timetableScraper(int timeframe, String login, String password, String arg) {
-
-        //  ! first row contains date !
-
-        List<List<String>> output = new ArrayList<>();
-
-        System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/EDriver/msedgedriver.exe");
-        WebDriver driver = new EdgeDriver(new EdgeOptions().addArguments(arg));
-
-        driver.get("https://extranet.vizja.net/");
-
-        WebElement loginEl = driver.findElement(By.name("login"));
-        WebElement passwordEl = driver.findElement(By.name("haslo"));
-
-        try {
-            loginEl.sendKeys(login);
-            passwordEl.sendKeys(password);
-        } catch (Exception e) {
-            driver.quit();
-            throw new NotFoundException("login data not found;\nHint: edit the config.properties file");
-        }
-        passwordEl.submit();
-
-        if (driver.getCurrentUrl().equals("https://extranet.vizja.net/")) {
-            driver.quit();
-            throw new IllegalArgumentException("Invalid login credentials; Could not login with given credentials");
-        }
-
-        WebElement date = driver.findElement(By.cssSelector("div.fc-center > h2"));
-        output.add(new ArrayList<>(List.of(date.getText())));
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(50));
-        WebElement next = driver.findElement(By.cssSelector(
-            "div.fc-button-group > button.fc-corner-right"));
-        WebElement timeTable = null;
-        int days = 0;
-        while (timeTable == null) {
-            try {
-                timeTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("table.fc-list-table")));
-            } catch (Exception e) {
-                next.click();
-                if (days > timeframe) {
-                    driver.quit();
-                    throw new NoSuchElementException("No timetable in specifiec timeframe.");
-                }
-                days += 1;
-            }
-        }
-
-        List<WebElement> lessons = timeTable.findElements(By.cssSelector("tr.fc-list-item"));
-        for (WebElement lesson : lessons) {
-            String time = lesson.findElement(By.cssSelector("td.fc-list-item-time")).getText();
-            String subject = lesson.findElement(By.cssSelector("td.fc-list-item-title > a")).getText();
-            output.add(new ArrayList<>(List.of(subject + " " + time)));
-        }
-
-        driver.quit();
-        return output;
-    }
-    
     private static String tryToGet(String property, Properties config) {
         try {
             String value = config.getProperty(property);
             return value;
         } catch (Exception e) {
             throw new NoSuchElementException("Unable to fetch " + property);
+        }
+    }
+
+    private static void tryToSet(String property, String value, Properties config) {
+        try {
+            config.setProperty(property, value);
+        } catch (Exception e) {
+            throw new NoSuchElementException("Unable to set " + property);
         }
     }
 
@@ -110,7 +55,16 @@ public class App {
                 String login = tryToGet("login", config);
                 String password = tryToGet("password", config);
                 String arg = tryToGet("arg", config);
-                List<List<String>> timetable = timetableScraper(Integer.parseInt(timeframe), login, password, arg);
+                List<List<String>> timetable = Scraper.timetableScraper(Integer.parseInt(timeframe), login, password, arg);
+
+                try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("timetable.bin"))) {
+                    os.writeObject(timetable);
+                } catch (FileNotFoundException e) {
+                    throw new FileNotFoundException("Unable to find timetable.bin file. It should be in src directory");
+                } catch (IOException e) {
+                    throw new IOException("Unable to write to timetable.bin file");
+                }
+                tryToSet("date", timetable.get(0).get(0), config);
             }
             else {
 
---
+++ src/Scraper.java
@@ -0,0 +1,71 @@
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class Scraper {
+    public static List<List<String>> timetableScraper(int timeframe, String login, String password, String arg) {
+
+        //  ! first row contains date !
+
+        List<List<String>> output = new ArrayList<>();
+
+        System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "/EDriver/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver(new EdgeOptions().addArguments(arg));
+
+        driver.get("https://extranet.vizja.net/");
+
+        WebElement loginEl = driver.findElement(By.name("login"));
+        WebElement passwordEl = driver.findElement(By.name("haslo"));
+
+        try {
+            loginEl.sendKeys(login);
+            passwordEl.sendKeys(password);
+        } catch (Exception e) {
+            driver.quit();
+            throw new NotFoundException("login data not found;\nHint: edit the config.properties file");
+        }
+        passwordEl.submit();
+
+        if (driver.getCurrentUrl().equals("https://extranet.vizja.net/")) {
+            driver.quit();
+            throw new IllegalArgumentException("Invalid login credentials; Could not login with given credentials");
+        }
+
+        WebElement date = driver.findElement(By.cssSelector("div.fc-center > h2"));
+        output.add(new ArrayList<>(List.of(date.getText())));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(50));
+        WebElement next = driver.findElement(By.cssSelector(
+            "div.fc-button-group > button.fc-corner-right"));
+        WebElement timeTable = null;
+        int days = 0;
+        while (timeTable == null) {
+            try {
+                timeTable = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("table.fc-list-table")));
+            } catch (Exception e) {
+                next.click();
+                if (days > timeframe) {
+                    driver.quit();
+                    throw new NoSuchElementException("No timetable in specifiec timeframe.");
+                }
+                days += 1;
+            }
+        }
+
+        List<WebElement> lessons = timeTable.findElements(By.cssSelector("tr.fc-list-item"));
+        for (WebElement lesson : lessons) {
+            String time = lesson.findElement(By.cssSelector("td.fc-list-item-time")).getText();
+            String subject = lesson.findElement(By.cssSelector("td.fc-list-item-title > a")).getText();
+            output.add(new ArrayList<>(List.of(subject + " " + time)));
+        }
+
+        driver.quit();
+        return output;
+    }
+}
