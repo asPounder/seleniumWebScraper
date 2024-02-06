d97e9cd9b9428dbf2d1cfe03448004753e63f7f8
asPounder <karlossokolowski75@gmail.com>
2024-02-06 20:18:57 +0100

    Refactoring #3

--- src/Timetable.java
+++ src/Timetable.java
@@ -1,55 +1,84 @@
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDate;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.util.Properties;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+
 public class Timetable {
     private String date;
     private String[][] timetableData;
-    private final String BINARY_PATH;
+    private String BINARY_PATH;
+    private String CONFIG_PATH;
 
-    public Timetable(String PATH) {
-        this.BINARY_PATH = PATH;
+    public Timetable(String BINARY_PATH, String CONFIG_PATH) {
+        this.BINARY_PATH = BINARY_PATH;
+        this.CONFIG_PATH = CONFIG_PATH;
     }
 
     public int load() {
         try (ObjectInputStream os = new ObjectInputStream(new FileInputStream(this.BINARY_PATH))) {
             List<List<String>> timetable = (List<List<String>>) os.readObject();
+            this.date = timetable.get(0).get(0);
+            this.timetableData = new String[timetable.size()-1][2];
+            for (int i = 1; i < timetable.size(); i++) {
+                this.timetableData[i-1][0] = timetable.get(i).get(0);
+                this.timetableData[i-1][1] = timetable.get(i).get(1);
+            }
         } catch (Exception e) {
             return -1;
         }
-
-        this.date = timetable.get(0).get(0);
-        this.timetableData = new String[timetable.size()-1][2];
-        for (int i = 1; i < timetable.size(); i++) {
-            this.timetableData[i-1][0] = timetable.get(i).get(0);
-            this.timetableData[i-1][1] = timetable.get(i).get(1);
-        }
-
         return 0;
     }
 
     public String getDate() {
-        return this.date; // throw something?
+        if (this.date == null) {
+            throw new IllegalStateException("Unable to get date. Call load() first.");
+        } else {
+            return this.date;
+        }
     }
 
     public String[][] getTimetableData() {
-        return this.timetableData; // throw something?
+        if (this.date == null) {
+            throw new IllegalStateException("Unable to get timetableData. Call load() first.");
+        } else {
+            return this.timetableData;
+        }
     }
 
-    public void set(final String CONFIG_PATH, final String DATE) {
+    public void set(final String DATE) throws IOException {
+        saveTimetableData();
+        saveTimetableDate(DATE);
+    }
+
+    private void saveTimetableData() throws IOException {
         try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(this.BINARY_PATH))) {
-            os.writeObject(timetable);
+            os.writeObject(this.timetableData);
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("Unable to find timetable.bin file. It should be in src directory");
         } catch (IOException e) {
             throw new IOException("Unable to write to timetable.bin file");
         }
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d LLLL yyyy", new Locale("pl"));
-        String timetableDate = LocalDate.parse(DATE, formatter).toString();
-        tryToSet("date", timetableDate, config);
-        try (FileOutputStream fo = new FileOutputStream(CONFIG_PATH)) {
-            config.store(fo, null);
-        } catch (FileNotFoundException e){
-            throw new FileNotFoundException("Could not locate the config.properties file");
-        }
     }
 
-    
+    private void saveTimetableDate(final String DATE) throws IOException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d LLLL yyyy", new Locale("pl"));
+        String timetableDate = LocalDate.parse(DATE, formatter).toString();
+
+        try (FileOutputStream fo = new FileOutputStream(this.CONFIG_PATH)) {
+            Properties config = new Properties();
+            config.setProperty("date", timetableDate);
+            config.store(fo, null);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Unable to find config.properties file. It should be in src directory");
+        } catch (IOException e) {
+            throw new IOException("Unable to write to config.properties file");
+        }
+    }
 }
