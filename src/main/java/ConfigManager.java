17b66b72eac5a5b1dd868d354c7920f78d436c56
asPounder <karlossokolowski75@gmail.com>
2024-03-10 19:44:29 +0100

    Migrated to Maven

---
+++ src/main/java/ConfigManager.java
@@ -0,0 +1,97 @@
+import java.util.Locale;
+import java.util.Properties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Class for managing configuration data stored in a properties file.
+ */
+public class ConfigManager {
+
+    /** The timeframe value read from the configuration file. */
+    public final int timeframe;
+    /** The login username read from the configuration file. */
+    public final String login;
+    /** The login password read from the configuration file. */
+    public final String password;
+    /** Additional argument for the web driver read from the configuration file. */
+    public final String arg;
+    /** The date read from the configuration file. */
+    public final String date;
+
+    /**
+     * Constructs a new Config object by reading configuration data from the specified file path.
+     *
+     * @param configPath The path to the configuration file.
+     * @throws IOException If an I/O error occurs while reading the configuration file.
+     */
+    public ConfigManager(final String configPath) throws IOException {
+        Properties config = new Properties();
+
+        try (final FileInputStream is = new FileInputStream(configPath)) {
+            config.load(is);
+            timeframe = Integer.parseInt(get("timeframe", config));
+            login = get("login", config);
+            password = get("password", config);
+            arg = get("arg", config);
+            date = get("date", config);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Could not find .properties file in specified path");
+        }
+    }
+
+    /**
+     * Saves the provided date to the configuration file.
+     *
+     * @param date       The date to be saved.
+     * @param configPath The path to the configuration file.
+     * @throws IOException If an I/O error occurs while writing to the configuration file.
+     */
+    public static void saveDate(final String date, final String configPath) throws IOException { // maybe LocalDate date?
+        Properties config = new Properties();
+        
+        try (final FileInputStream is = new FileInputStream(configPath)) {
+            config.load(is);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Could not find .properties file in specified path");
+        }
+
+        config.setProperty("date", date);
+
+        try (final FileOutputStream os = new FileOutputStream(configPath)) {
+            config.store(os, null);
+        } catch (IOException e) {
+            throw new IOException("Could not update date in .properties file.");
+        }
+    }
+
+    /**
+     * Formats the provided date string to a LocalDate object.
+     *
+     * @param date The date string to be formatted.
+     * @return The formatted LocalDate object.
+     */
+    public static LocalDate formatToLocalDate(final String date) {
+        if (date.equals("")) {
+            return null;
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d LLLL yyyy", new Locale("pl"));
+        LocalDate formatted = LocalDate.parse(date, formatter);
+
+        return formatted;
+    }
+
+    private String get(final String property, final Properties config) {
+        String value = config.getProperty(property);
+        if (value != null) {
+            return value;
+        } else {
+            throw new IllegalArgumentException("Property not found: " + property);
+        }
+    }
+}
---
+++ src/main/java/TimetableUtils.java
@@ -0,0 +1,59 @@
+import java.io.ObjectInputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.util.List;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+/**
+ * Utility class for serializing, deserializing, and formatting timetable data.
+ */
+public class TimetableUtils {
+
+    /**
+     * Deserialize timetable data from a binary file.
+     *
+     * @param binaryPath The path to the binary file containing the serialized timetable data.
+     * @return The deserialized timetable data as a two-dimensional array of strings, or {@code null} if an error occurs.
+     */
+    public static String[][] deserializeTimetable(final String binaryPath) {
+        try (ObjectInputStream os = new ObjectInputStream(new FileInputStream(binaryPath))) {
+            return (String[][]) os.readObject();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Format a timetable from a list of lists to a two-dimensional array.
+     *
+     * @param timetable The timetable data as a list of lists, where each inner list represents a row of the timetable.
+     * @return The formatted timetable data as a two-dimensional array of strings.
+     */
+    public static String[][] formatTimetable(List<List<String>> timetable) {
+        String[][] formatted = new String[timetable.size()-1][2];
+        for (int i = 1; i < timetable.size(); i++) {
+            formatted[i-1][0] = timetable.get(i).get(0);
+            formatted[i-1][1] = timetable.get(i).get(1);
+        }
+        return formatted;
+    }
+
+    /**
+     * Serialize timetable data to a binary file.
+     *
+     * @param timetable  The timetable data to be serialized, represented as a two-dimensional array of strings.
+     * @param binaryPath The path to the binary file where the serialized timetable data will be written.
+     * @throws IOException If an I/O error occurs while writing the data to the file.
+     */
+    public static void serializeTimetable(final String[][] timetable, final String binaryPath) throws IOException {
+        try (ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(binaryPath))) {
+            os.writeObject(timetable);
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("Unable to find timetable.bin file. It should be in src directory");
+        } catch (IOException e) {
+            throw new IOException("Unable to write to timetable.bin file");
+        }
+    }
+}
